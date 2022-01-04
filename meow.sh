82cb1397087a7fe16866d447d4786a5c7bc91a61
Tyler Nijmeh <tylernij@gmail.com>
2022-01-04 02:41:54 -0800

    Update meow.sh

--- meow.sh
+++ meow.sh
@@ -11,7 +11,7 @@ CONFIG=".config"
 SEP="~"
 OUTPUT_FORMAT="%(title)s $SEP %(id)s.%(ext)s"
 UPDATE_URL="https://raw.githubusercontent.com/tytydraco/meow/main/meow.sh"
-ENV_VARIABLES=("URL" "FORMAT" "VIDEO" "PPARGS" "COOKIES")
+ENV_VARIABLES=("URL" "VIDEO" "PPARGS" "COOKIES")
 
 FEATURE_ARIA2=false
 
